9f62c05580b1d6c59553efffc53d277d8bee3f7e
liuqingyong <3476504128@qq.com>
2018-10-31 20:20:20 +0800

    helloworld add commit

--- equation.c
+++ equation.c
@@ -2,3 +2,14 @@
 //ax + by = c
 //dx + ey = f
 //æ³¨æ„è¾“å…¥/è¾“å‡ºï¼Œæ•°æ®ç±»å‹
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	double a, b, c, d, e, f, x, y;
+	printf("ÊäÈëa£¬b£¬c£¬d£¬e£¬f");
+	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&e,&f);
+	x=(c*e-b*f)/(a*e-b*d);
+	y=(c*d-a*f)/(b*d-a*e);
+	printf("x=%lf,y=%lf",x,y);
+ }
--- equation1.c
+++ equation1.c
@@ -1,3 +1,42 @@
 //æ±‚è§£ä¸€å…ƒäºŒæ¬¡æ–¹ç¨‹çš„æ ¹
 //æŸ¥é˜…sqrt()å‡½æ•°
 //æ³¨æ„æœ‰æ²¡æœ‰è§£
+#include<stdio.h>
+#include<math.h>
+void CalculateRealRoot(double a1, double b1, double c1);
+int main(void)
+{   double a,b,c;
+	printf("a=");
+	scanf("%lf",&a);
+	printf("b=");
+	scanf("%lf",&b);
+	printf("c=");
+	scanf("%lf",&c);
+	CalculateRealRoot(a,b,c);
+	return 0;
+}
+void CalculateRealRoot(double a1, double b1, double c1)
+{
+	double delta, x1, x2;
+	delta=b1*b1-4*a1*c1;
+	if (delta<0)
+    {
+	printf("ÎŞÊµ¸ù.\n");
+    }
+    else
+    {
+	if (delta==0)
+	{
+		x1=-b1/(2*a1);
+		x2=-b1/(2*a1);
+		printf("x1=%lf,x2=%lf",x1,x2);
+	}
+	else
+	{
+		x1=(-b1+sqrt(delta))/(2*a1);/*sqrt()º¯ÊıÇóÒ»¸öÊµÊıµÄÆ½·½¸ù£¬sqrt()º¯ÊıÊÇÔÚ"math.h"Í·ÎÄ¼şÖĞÉùÃ÷µÄ*/ 
+		x2=(-b1-sqrt(delta))/(2*a1);
+		printf("x1=%lf,x2=%lf",x1,x2);
+	}
+    }
+} 
+
