2027567d9013f07701a727ec9fc424bb7d48ef2c
liuqingyong <3476504128@qq.com>
2018-10-31 21:00:25 +0800

    helloworld add commit

--- equation.c
+++ equation.c
@@ -6,10 +6,22 @@
 #include<math.h>
 main()
 {
-	double a, b, c, d, e, f, x, y;
+	double a, b, c, d, e, f, x, y,t1,t2,t3,t4;
 	printf("输入a，b，c，d，e，f");
 	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&e,&f);
 	x=(c*e-b*f)/(a*e-b*d);
 	y=(c*d-a*f)/(b*d-a*e);
+	t1=(a*e-b*d);
+	t2=(b*d-a*e);
+	t3=b/a;
+	t4=e/d;
+	if(t1 !=0 && t2!=0)
 	printf("x=%lf,y=%lf",x,y);
- }
+    else
+    if (t1==0&&c!=f)
+    printf("无解");
+	else
+	if(t3==t4)
+	printf("有无穷多组解");
+	return 0; 
+ } 
