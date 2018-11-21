ba7955fbf3f061edcd725eb5a418d1858638741b
liuqingyong <3476504128@qq.com>
2018-11-21 19:57:08 +0800

    helloworld add commit

--- BasicPrime.c
+++ BasicPrime.c
@@ -1,5 +1,21 @@
 //ç´ æ•°æ˜¯åªæœ‰1å’Œæœ¬èº«æ˜¯å› å­çš„æ•°
 //2018/11/14 æ›´æ–°
+#include<stdio.h>
 int main() {
     //to do sth
+ int x,i,m;
+ scanf("%d",&x);
+   for(i=2;i<x;i++)
+     {
+	  if(x%i==0)
+          {
+                 printf("%d²»ÊÇËØÊı",x);
+                 break;
+	      }
+     else
+     m=i;
+     }
+    if(m==x-1) 
+    printf("%dÊÇËØÊı",x);
+ return 0;
 }
